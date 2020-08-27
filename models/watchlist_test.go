7d41aa8c2ed10657b780c131903515c6278e1015
gfleury <gfleury@gmail.com>
2020-08-27 22:20:32 +0200

    Testing other watchlists

--- models/watchlist_test.go
+++ models/watchlist_test.go
@@ -6,9 +6,10 @@ func (s *ModeledApiSuite) TestGetWatchlist(c *check.C) {
 	wl, err := s.ma.GetWatchlist(&WatchlistRequest{
 		Arguments: Arguments{
 			WatchlistID: "2491746",
-			AssetTypes:  "Bond,Stock,StockIndex,CfdOnStock,CfdOnIndex,FxSpot,FxForwards,FxVanillaOption,FxKnockInOption,FxKnockOutOption,FxOneTouchOption,FxNoTouchOption",
-			Index:       0,
-			RowCount:    20,
+			// WatchlistID: "2452785",
+			AssetTypes: "Bond,Stock,StockIndex,CfdOnStock,CfdOnIndex,FxSpot,FxForwards,FxVanillaOption,FxKnockInOption,FxKnockOutOption,FxOneTouchOption,FxNoTouchOption",
+			Index:      0,
+			RowCount:   20,
 		},
 		RefreshRate: 500,
 		Format:      "application/json",

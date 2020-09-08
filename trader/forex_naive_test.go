df4e1b9e525ba212ef2d3de3ab4c8a750eac2812
gfleury <gfleury@gmail.com>
2020-09-08 22:13:10 +0200

    Adding pre-forex broken

---
+++ trader/forex_naive_test.go
@@ -0,0 +1,25 @@
+// +build integration
+// +build !unit
+
+package trader
+
+import (
+	"github.com/gfleury/narrowTrade/analysis"
+	check "gopkg.in/check.v1"
+)
+
+func (s *Suite) TestBuyForexNaive_5_6(c *check.C) {
+	t := ForexNaive{
+		StockNaive: &StockNaive{
+			BasicSaxoTrader: &BasicSaxoTrader{
+				AccountKey:         s.acc.GetAccountKeyMe(),
+				ModeledAPI:         s.ma,
+				InstrumentAnalyser: &analysis.DummyAnalyser{},
+			},
+		},
+	}
+
+	// Buy USDEUR 5/6 ticks
+	err := t.Trade(TradeParameter{"", []int{22}, 5, 6, 0})
+	c.Assert(err, check.IsNil)
+}

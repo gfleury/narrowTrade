b221b4e3bada3eca691397df9cac8f0ffc25a3a0
gfleury <gfleury@gmail.com>
2020-10-09 00:17:26 +0200

    Adding missing file

---
+++ utils/cache_entry.go
@@ -0,0 +1,24 @@
+package utils
+
+import "time"
+
+type CacheEntry struct {
+	Value  interface{}
+	Expire time.Time
+}
+
+type Cache map[string]*CacheEntry
+
+func (c Cache) Put(k string, v interface{}, ex time.Duration) {
+	c[k] = &CacheEntry{v, time.Now().Add(ex)}
+}
+
+func (c Cache) Get(k string) interface{} {
+	if ce, ok := c[k]; ok {
+		if !time.Now().After(ce.Expire) {
+			return ce.Value
+		}
+		c[k] = nil
+	}
+	return nil
+}
